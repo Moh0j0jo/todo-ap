d77769a0da809bbae3f9f3f7e375d0b7d1cc2084
Moh0j0jo <peter.pinnel@gmail.com>
2024-02-27 14:53:33 +0100

    css rule

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,14 @@ const App = () => {
 
   const [todoList, setTodoList] = useState([]);
   const [isDark, setIsDark] = useState(true);
-  const [ title, setTitle ] = useState('')
+  const [title, setTitle] = useState('')
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       const querySnapshot = await getDocs(collection(db, 'todoList'));
       const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setCount(data.length)
       setTodoList(data);
     };
 
@@ -47,14 +48,20 @@ const App = () => {
   const onSubmitTask = async (title) => {
     try {
       await addDoc(collection(db, 'todoList'), { title, done: false });
-      setTodoList(prevTodoList => [...prevTodoList, { title, done: false }]);
+      setTodoList(prevTodoList => [
+        ...prevTodoList, 
+        { 
+          title,
+          done: false 
+        }
+      ]);
     } catch (error) {
       console.error('Error adding task:', error);
     }
   };
 
   const toggleTheme = () => {
-    setIsDark(prevIsDark => !prevIsDark);
+    setIsDark(!isDark);
   };
 
   return (
@@ -65,13 +72,13 @@ const App = () => {
           <h1>TODO</h1>
           <div className="icon"
             onClick={toggleTheme}>
-            {isDark ? 
-              <svg xmlns="http://www.w3.org/2000/svg" 
+            {isDark ?
+              <svg xmlns="http://www.w3.org/2000/svg"
                 width="26" height="26"><path fill="#FFF" fillRule="evenodd" d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z" />
               </svg>
-            :
-              <svg xmlns="http://www.w3.org/2000/svg" 
-                width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"/>
+              :
+              <svg xmlns="http://www.w3.org/2000/svg"
+                width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z" />
               </svg>
             }
 
@@ -85,9 +92,9 @@ const App = () => {
 
           <input
             type="text"
-            value = {title}
+            value={title}
             placeholder="Create a new todo ..."
-            onChange={e=> setTitle(e.target.value)}
+            onChange={e => setTitle(e.target.value)}
           />
 
           <button className="submit-button"
@@ -100,25 +107,25 @@ const App = () => {
       </nav>
 
       <ul className="task-list">
-      {todoList.map((todoItem) => (
-        <div 
-          key={todoItem.id}
-          className="list-item"
-        >
-          <Checkbox
-                      id={todoItem.id}
-                      title={todoItem.title}
-                      checked={todoItem.done}
-                      onChange={(newCheckedState) => handleCheckboxChange(todoItem.id, newCheckedState)}
-          />
-          <button
-            className='delete-button'
-            onClick={() => deleteTask(todoItem.id)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" /></svg>
-        </button>
-        </div>
-      ))}
-        <div 
+        {todoList.map((todoItem) => (
+          <div
+            key={todoItem.id}
+            className="list-item"
+          >
+            <Checkbox
+              id={todoItem.id}
+              title={todoItem.title}
+              checked={todoItem.done}
+              onChange={(newCheckedState) => handleCheckboxChange(todoItem.id, newCheckedState)}
+            />
+            <button
+              className='delete-button'
+              onClick={() => deleteTask(todoItem.id)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" /></svg>
+            </button>
+          </div>
+        ))}
+        <div
           className="summary">
           <p>{`${count}  items left`}</p>
           <button>Clear Completed</button>
